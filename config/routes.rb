232d8c931811cc6d01b4015cdf09ff33b5f955d7
Jules Prevost <jprevostdev@gmail.com>
2020-06-22 17:15:05 +0200

    routes + validations ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
+
+  resources :creeks, only: [ :show, :new, :create, :update, :destroy ] do
+    resources :tickets, only: [ :create ]
+  end
 end
