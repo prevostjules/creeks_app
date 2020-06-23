1127b54df35d8065528eb419c48b7a9bfe031660
Jules Prevost <jprevostdev@gmail.com>
2020-06-23 18:44:59 +0200

    -api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,25 +2,5 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
   include HTTParty
   def home
-KEY
-VALUE
-DESCRIPTION
-        headers = {
-          "Content-Type" => "application/x-www-form-urlencoded"
-        }
-        body = {
-          code: '4/1AEWIy05Y-5Ksg09pqJRbbcqMLIkKre9UsHea_j_Tjox_PFIBUrOw8QUVMLEkJJJPVjijo6HVp411CSPpS3XVsc',
-          client_id: '206719754391-rer1a7fdjuf8g80sq4l1ab55bptsc2n7.apps.googleusercontent.com',
-          client_secret: 'EgetGE2RJCFomBQu3N3cV5hP',
-          redirect_uri: 'http://localhost:3000',
-          grant_type: 'authorization_code'
-        }.to_json
-
-      result = HTTParty.post('https://accounts.google.com/o/oauth2/token',
-        :headers => headers,
-        :body => body)
-4/1AEWIy05Y-5Ksg09pqJRbbcqMLIkKre9UsHea_j_Tjox_PFIBUrOw8QUVMLEkJJJPVjijo6HVp411CSPpS3XVsc
-      byebug
-      puts result
   end
 end
