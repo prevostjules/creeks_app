214e07b51a1dc8df882d451188196041bdc822d6
AntoineBarck <antoine.barck@gmail.com>
2020-06-25 12:01:14 +0200

    studio

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -33,3 +33,11 @@ document.addEventListener('turbolinks:load', () => {
   // initSelect2();
   hasGetUserMedia();
 });
+
+import { loadDynamicBannerText } from '../components/banner';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  // [...]
+  loadDynamicBannerText();
+});
