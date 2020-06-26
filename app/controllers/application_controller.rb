24aa56fd66202073710ec8e7d5bace2fe389059f
Jules Prevost <jprevostdev@gmail.com>
2020-06-26 15:32:15 +0200

    finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :role])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:description])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :role, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:description, :photo])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
     @creek = Creek.new
     @user = User.find(params[:id])
     @link_to_google_auth = "https://accounts.google.com/o/oauth2/auth?client_id=#{ENV['CLIENT_ID']}&redirect_uri=#{ENV['REDIRECT_URI']}&scope=https://www.googleapis.com/auth/youtube&response_type=code&access_type=offline"
+    @tickets = @user.tickets
   end
 end
