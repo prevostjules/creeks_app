953ef2c115d8d360c9a87d0aee4ba03645b0c48f
Jules Prevost <jprevostdev@gmail.com>
2020-06-30 13:47:18 +0200

    t

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_082256) do
+ActiveRecord::Schema.define(version: 2020_06_30_113512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,16 @@ ActiveRecord::Schema.define(version: 2020_06_30_082256) do
     t.index ["user_id"], name: "index_creeks_on_user_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "creek_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["creek_id"], name: "index_messages_on_creek_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "tickets", force: :cascade do |t|
     t.bigint "creek_id", null: false
     t.bigint "user_id", null: false
@@ -93,6 +103,8 @@ ActiveRecord::Schema.define(version: 2020_06_30_082256) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "creeks", "categories"
   add_foreign_key "creeks", "users"
+  add_foreign_key "messages", "creeks"
+  add_foreign_key "messages", "users"
   add_foreign_key "tickets", "creeks"
   add_foreign_key "tickets", "users"
   add_foreign_key "users", "categories"
