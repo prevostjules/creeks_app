d617e6fead69ff61e1316bae75371de8483f3d34
matthieu hedin <hedin.matthieu75@gmail.com>
2020-06-30 10:25:19 +0200

    une belle navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_153850) do
+ActiveRecord::Schema.define(version: 2020_06_30_082256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 2020_06_25_153850) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "creeks", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -47,8 +53,9 @@ ActiveRecord::Schema.define(version: 2020_06_25_153850) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "iframe"
     t.string "id_broadcast"
-    t.string "category"
     t.integer "duration"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_creeks_on_category_id"
     t.index ["user_id"], name: "index_creeks_on_user_id"
   end
 
@@ -77,12 +84,16 @@ ActiveRecord::Schema.define(version: 2020_06_25_153850) do
     t.string "refresh_token"
     t.string "stream_id"
     t.string "stream_name"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_users_on_category_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "creeks", "categories"
   add_foreign_key "creeks", "users"
   add_foreign_key "tickets", "creeks"
   add_foreign_key "tickets", "users"
+  add_foreign_key "users", "categories"
 end
