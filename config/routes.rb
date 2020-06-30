cee0c5c036f27f94cd6f62cb9c6459da107cc1dc
matthieu hedin <hedin.matthieu75@gmail.com>
2020-06-30 10:50:09 +0200

    model

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
   resources :pages, only: [:index]
+  resources :categories, only: [:index]
 
   resources :creeks, only: [ :show, :new, :create, :update, :destroy, :edit ] do
     resources :tickets, only: [ :create ]
