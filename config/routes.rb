1c539d0bc14bbdadc679cdfc00bb6c7c8533f860
AntoineBarck <antoine.barck@gmail.com>
2020-06-30 10:12:06 +0200

    staff

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/team", to: "pages#team", as: "team"
+
   resources :users, only: [:show]
   resources :pages, only: [:index]
 
