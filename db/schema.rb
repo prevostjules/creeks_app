e4b9c7d57b256763fca31ca6800030bacb03d7fd
matthieu hedin <hedin.matthieu75@gmail.com>
2020-07-01 10:31:51 +0200

    priez

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,8 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2020_06_30_113512) do
+
+ActiveRecord::Schema.define(version: 2020_07_01_074224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
